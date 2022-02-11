879288efdc5c50b8b444d3c4815428e2f2e248d0
Tekrodz <kevinkmmichel@gmail.com>
2022-02-11 12:46:50 +0100

    :iphone: Responsive

---
+++ UX-Tech-Web-master/README.md
@@ -0,0 +1,1 @@
+# UX-Tech-Web
---
+++ UX-Tech-Web-master/index.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://kit.fontawesome.com/28c63ecd3f.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css">
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WP7QFXC');</script>
+<!-- End Google Tag Manager -->
+  <title>Les formations Coding & Digital Innovation</title>
+</head>
+<body>
+      <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WP7QFXC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+  <div class="deco-top">
+    <div class='dotted-circle'>
+      <div class='circle'>
+        <img src='img/Logo_IIM_fd_blanc.png'>
+      </div>
+    </div>
+    <h1>Découvrir les formations Coding & Digital Innovation </h1>
+  </div>
+
+
+  <section class="first">
+    <div class='part1'>
+      <div class='learn'>
+          <h2>Apprenez à faire vos propres sites Web et les mettres en lignes.</h2>
+          <p> Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <div class='button'>
+          <a class='button_orange' href ="#form">S'inscrire</a>
+          <a class='button_white' href ="#formations">Voir nos formations</a>
+        </div>
+      </div>
+      <div class='test'> 
+        <div class='circle1'>
+          <div class='relative rond'>
+            <i class="fab fa-css3-alt fa-3x "></i>
+          </div>
+          <div class='relative2 rond'>
+            <i class="fab fa-js fa-3x "></i>
+          </div>
+          <div class='relative3 rond'>
+            <i class="fab fa-php fa-3x "></i>
+          </div>
+          <div class='circle2'>
+            <div class='relative4 rond2'>
+              <i class="fab fa-html5 fa-2x"></i>
+            </div>
+            <div class='relative5 rond2'>
+              <i class="fab fa-python fa-2x"></i>
+            </div>
+            <div class='circle3'>
+              <div class='circle4'><p>💻</p></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section class="second" id="formations">
+    <!-- <button id="previous">Previous</button> -->
+    <div class="slider">
+    
+    <article class="card-back-two left">
+        <div class="card-top">
+          <i class="fab fa-css3 fa-lg"></i>
+          <div>
+            <h3>Base CSS</h3>
+            <p>Styliser une page web</p>
+          </div>
+        </div>
+        <div class="card-middle">
+          
+        </div>
+        <div class="card-bottom">
+          <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+      </article>
+      <article class="card-back-one left">
+        <div class="card-top">
+          <i class="fab fa-css3 fa-lg"></i>
+          <div>
+            <h3>Base CSS</h3>
+            <p>Styliser une page web</p>
+          </div>
+        </div>
+        <div class="card-middle">
+          
+        </div>
+        <div class="card-bottom">
+          <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+      </article>
+
+      <article class="card">
+        <div class="card-top">
+          <i class="fab fa-css3 fa-lg"></i>
+          <div>
+            <h3>Base CSS</h3>
+            <p>Styliser une page web</p>
+          </div>
+        </div>
+        <div class="card-middle">
+          
+        </div>
+        <div class="card-bottom">
+          <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+      </article>
+
+      <article class="card-back-one right">
+        <div class="card-top">
+          <i class="fab fa-css3 fa-lg"></i>
+          <div>
+            <h3>Base CSS</h3>
+            <p>Styliser une page web</p>
+          </div>
+        </div>
+        <div class="card-middle">
+          
+        </div>
+        <div class="card-bottom">
+          <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+      </article>
+
+      <article class="card-back-two right">
+        <div class="card-top">
+          <i class="fab fa-css3 fa-lg"></i>
+          <div>
+            <h3>Base CSS</h3>
+            <p>Styliser une page web</p>
+          </div>
+        </div>
+        <div class="card-middle">
+          
+        </div>
+        <div class="card-bottom">
+          <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          </p>
+        </div>
+      </article>
+
+    </div>
+    <!-- <button id="next">Next</button> -->
+  </section>
+
+  <section class="third" id="form">
+    <h2>S'inscrire à nos formations</h2>
+    <form action="" method="POST">
+        <input type="text" name="firstname" placeholder="Votre prénom.." required>
+        <input type="text" name="lastname" placeholder="Votre nom de famille..." required>
+        <input type="email" name="email" placeholder="Votre email..." required>
+        <select name="formation" required>
+          <option value="">Selectionner une formation</option>
+        </select>
+        <button type="submit">Envoyer</button>
+    </form>
+  </section>
+
+  <footer>
+        <div class="footer_symbole">
+            <article class="footer_symbole_article">
+              <i class="far fa-handshake fa-7x "></i>
+                <h3 class='descend'>Ils nous font confiance</h3>
+                <p class="footer_symbole_texte"> Plus de 20 grandes entreprisesnous font confiance</p>
+            </article>
+            <article class="footer_symbole_article">
+                <i class="fas fa-trophy fa-7x"></i>
+                <h3 class='descend'>Numéro 1 des écoles française du digital</h3>
+                <p class="footer_symbole_texte">Les 80% de nos élèves trouvent un travail en 3 mois
+                    leur diplome avec un salaire moyen de 42k € par an.</p>
+            </article>
+            <article class="footer_symbole_article ">
+                <i class="fas fa-graduation-cap fa-7x"></i>
+                <h3 class='descend'>2 Mastères reconnu par l’état</h3>
+                <p class="footer_symbole_texte">Nos élèves auront le choix entre
+                    Ingénierie web et mobile
+                    et un double diplôme
+                    Management de la transformation digitale
+                </p>
+            </article>
+        </div>
+        <div class="footer_mentions">
+            <div class="footer_mentions_title">
+                <h2>Mentions légales</h2>
+            </div>
+            <div class="footer_mentions_hr">
+                <div class="foooter_hr"></div>
+            </div>
+            <div class="footer_mentions_text">
+                <p>Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions.
+<br><br>
+Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par courriel, à l’adresse contact@anthedesign.fr, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).
+<br><br>
+Tout contenu téléchargé se fait aux risques et périls de l’utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d’un quelconque dommage subi par l’ordinateur de l’utilisateur ou d’une quelconque perte de données consécutives au téléchargement.
+<br><br>
+De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.
+<br><br>
+Les liens hypertextes mis en place dans le cadre du présent site internet en direction d’autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de SARL ANTHEDESIGN.</p>
+<br><br>
+            </div>
+        </div>
+    </footer>
+    <script>
+      window.axeptioSettings = {
+      clientId: "620646cdf9131a52060ec28a",
+      cookiesVersion: "ux-fcbouffs-fr",
+      
+      };
+
+      (function(d, s) {
+        var t = d.getElementsByTagName(s)[0], e = d.createElement(s);
+        e.async = true; e.src = "//static.axept.io/sdk.js";
+        t.parentNode.insertBefore(e, t);
+      })(document, "script");
+    </script>
+</body>
+</html>
+<?php 
